4a4a2e67e381a05b84c5946f2ea05fc62e596df9
SmeLros <2762891808@qq.com>
2024-06-05 20:10:36 +0800

    🐎 ci(mdx): 新增mdx组件及其配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import createMDX from '@next/mdx'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js']
+}
+
+const withMDX = createMDX({
+    extension: /\.mdx?$/,
+    // Add markdown plugins here, as desired
+})
+ 
+export default withMDX(nextConfig)
