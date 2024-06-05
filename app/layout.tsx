e8298ac2818908caa8fc6b7d3b82b4c9bd8a4683
SmeLros <2762891808@qq.com>
2024-06-05 20:21:05 +0800

    🦄 refactor(layout): 调整根页面布局

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { DocsAside } from "@/components/docs/Aside";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="w-screen h-screen flex">
+          <DocsAside />
+          <main className="flex-1">
+            {children}
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
